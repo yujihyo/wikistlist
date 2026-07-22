8e48accfb56b07e75936a24ad72a42ab31417e93
yujihyo <rgh822@naver.com>
2026-07-22 13:30:10 +0900

    0

--- js/main.js
+++ js/main.js
@@ -434,3 +434,20 @@ img{
     }
 
 }
+
+const sidebar =
+    document.getElementById("sidebar");
+
+const sidebarToggle =
+    document.getElementById("sidebarToggle");
+
+sidebarToggle.addEventListener("click",()=>{
+
+    sidebar.classList.toggle("closed");
+
+    sidebarToggle.textContent =
+        sidebar.classList.contains("closed")
+        ? ">"
+        : "<";
+
+});
